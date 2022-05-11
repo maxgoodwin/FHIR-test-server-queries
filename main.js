bfd9f4b678842b699f84eac6661a214425f5d961
Max Goodwin <maxgoodwin3@pm.me>
2022-05-11 16:11:50 +1000

    Changed codes

--- main.js
+++ main.js
@@ -2,20 +2,19 @@ const axios = require('axios');
 const util = require('util');
 
 const baseUrl = 'https://r4.ontoserver.csiro.au/fhir';
-const ankleAmputationCode = 397218006;
-const disarticulationCode = 15440009;
+const sprainedAnkleCode = 44465007;
+const ankleLigamentInjuryCode = 240019006;
 const system = 'http://snomed.info/sct';
 const format = 'json';
-const snomedCodeLookupURL = `${baseUrl}/CodeSystem/$lookup?system=${system}&code=${ankleAmputationCode}&_format=${format}`;
-const snomedValueSetExpandURL = `${baseUrl}/ValueSet/$expand?url=${system}?fhir_vs=isa/${disarticulationCode}`;
+const snomedCodeLookupURL = `${baseUrl}/CodeSystem/$lookup?system=${system}&code=${sprainedAnkleCode}&_format=${format}`;
+const snomedValueSetExpandURL = `${baseUrl}/ValueSet/$expand?url=${system}?fhir_vs=isa/${ankleLigamentInjuryCode}`;
 
-const getSnomedCodeLookup = async (url) => {
+const getSnomedCodeLookup = async (url, snomedCode) => {
     const response = await axios.get(url);
-    // We can retrieve server infor from the headers of any request
+    // We can retrieve server info from the response headers of any request
     console.log('~~~~~ Server/data version: ' + response.headers['x-powered-by'] + ' ~~~~~');
     console.log();
-    console.log(`~~~~~ Snomed code ${ankleAmputationCode} name: 
-        ${util.inspect(response.data.parameter[0].valueString, false, null, true)} ~~~~~`);
+    console.log(`~~~~~ Snomed code ${snomedCode} name: ${util.inspect(response.data.parameter[0].valueString, false, null, true)} ~~~~~`);
     console.log();
 }
 
@@ -27,5 +26,5 @@ const getSnomedValueSet = async (url, valueSetName) => {
     console.log();
 }
 
-getSnomedCodeLookup(snomedCodeLookupURL);
-getSnomedValueSet(snomedValueSetExpandURL, 'disarticulation');
+getSnomedCodeLookup(snomedCodeLookupURL, sprainedAnkleCode);
+getSnomedValueSet(snomedValueSetExpandURL, 'lesion of ligaments of the ankle region');
